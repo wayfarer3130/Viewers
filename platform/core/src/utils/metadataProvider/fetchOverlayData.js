a6b46a1742d360596b587c9838385515d3199805
Bill Wallace <wayfarer3130@gmail.com>
2021-10-22 12:38:05 -0400

    fix(OHIF): Added a custom overlay colour, and allow relative URL in BulkDataURI

--- platform/core/src/utils/metadataProvider/fetchOverlayData.js
+++ platform/core/src/utils/metadataProvider/fetchOverlayData.js
@@ -62,12 +62,12 @@ async function _getOverlayData(tag, server) {
 
   // TODO: Workaround for dcm4chee behind SSL-terminating proxy returning
   // incorrect bulk data URIs
-  if (server.qidoRoot.indexOf('https') === 0 && !uri.includes('https')) {
+  if (server.wadoRoot.indexOf('https') === 0 && !uri.includes('https')) {
     uri = uri.replace('http', 'https');
   }
 
-  if (!uri.includes('http')) {
-    uri = server.qidoRoot + '/studies/' + uri;
+  if (!uri.includes(':') && !uri.startsWith('/')) {
+    uri = server.wadoRoot + 'studies/' + uri;
   }
 
   const config = {
